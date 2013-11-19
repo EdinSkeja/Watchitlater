b12f0bd61a503deab11eaf70aa99df4b9de44e57
Edin Skeja <edin.skeja@me.com>
2013-11-19 09:46:24 -0500

    putting in all files

---
+++ readme.md
@@ -0,0 +1,3 @@
+## What is This?
+
+This is my project for the course 2DV407 (RIA-Development With JavaScript).
---
+++ scripts/views/app.js
@@ -0,0 +1,14 @@
+define(['backbone'], function(Backbone) {
+	var App = Backbone.View.extend({
+        el: $('#hello_container'),
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            var variables = { hello: "Hello World!" };
+            var template = _.template( $("#hello_template").html(), variables );
+            $(this.el).html(template);
+        }
+	});
+	return App;
+});
